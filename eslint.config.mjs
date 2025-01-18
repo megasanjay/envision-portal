3f400d2e6a1636536c824ce5d7d407a1096e38a1
Sanjay Soundarajan <sanjay.16@live.com>
2025-01-17 18:15:33 -0800

    initial: first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  rules: {
+    "vue/padding-line-between-tags": [
+      "error",
+      [{ blankLine: "always", prev: "*", next: "*" }],
+    ],
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/ui", "@nuxt/eslint", "nuxt-auth-utils"],
+  devtools: { enabled: true },
+  compatibilityDate: "2025-01-16",
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: "double",
+        semi: true,
+      },
+    },
+  },
+});
